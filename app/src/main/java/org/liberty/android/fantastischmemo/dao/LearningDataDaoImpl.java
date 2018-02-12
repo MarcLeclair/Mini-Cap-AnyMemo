b22634619c006a450d0f21401102c61ee31effc7
omarhammoud <omar_hammoud96@hotmail.com>
2018-02-12 12:55:38 -0500

    added comment to DAO file

--- app/src/main/java/org/liberty/android/fantastischmemo/dao/LearningDataDaoImpl.java
+++ app/src/main/java/org/liberty/android/fantastischmemo/dao/LearningDataDaoImpl.java
@@ -62,6 +62,7 @@ public class LearningDataDaoImpl extends AbstractHelperDaoImpl<LearningData, Int
         ld.setGrade(5);
         update(ld);
     }
+    // The card will be shown again right away if marked as favourite
     public void markAsFavourite(LearningData ld) {
         ld.setNextLearnDate(new Date(0));
         ld.setFirstLearnDate(new Date());
