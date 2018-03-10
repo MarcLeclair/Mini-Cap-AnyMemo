8ed47a10f0ecab5fb52e1ea5e917dc537baf2f91
Marc Leclair <marc.leclair0113@gmail.com>
2018-03-10 16:04:24 -0500

    -Updated db version to 9 and added onupgrade check

--- app/src/main/java/org/liberty/android/fantastischmemo/common/AnyMemoDBOpenHelper.java
+++ app/src/main/java/org/liberty/android/fantastischmemo/common/AnyMemoDBOpenHelper.java
@@ -31,7 +31,7 @@ public class AnyMemoDBOpenHelper extends OrmLiteSqliteOpenHelper {
 
     private final String dbPath;
 
-    private static final int CURRENT_VERSION = 5;
+    private static final int CURRENT_VERSION = 9;
 
     private CardDao cardDao = null;
 
@@ -157,26 +157,36 @@ public class AnyMemoDBOpenHelper extends OrmLiteSqliteOpenHelper {
         }
         if (oldVersion <= 4) {
             try {
-
                 database.execSQL("alter table learning_data add column firstLearnDate VARCHAR");
                 database.execSQL("update learning_data set firstLearnDate='2010-01-01 00:00:00.000000'");
                 database.execSQL("alter table settings add column learningMode INTEGER");
                 database.execSQL("update settings set learningMode='0'");
-                database.execSQL("alter table cards add column learningDate VARCHAR");
-                database.execSQL("update cards set learningDate='0000-00-00 00:00:00.000000'");
 
-                database.execSQL("alter table cards add column imgPath VARCHAR");
-                database.execSQL("update cards set imgPath=''");
+
 
                 Log.d(TAG, "Adding  new column called favourite for db version " + oldVersion);
-                database.execSQL("alter table learning_data add column favourite INTEGER");
-                database.execSQL("update learning_data set favourite='0'");
+
 
             } catch (android.database.SQLException e) {
                 Log.e(TAG, "Upgrading failed, the column firstLearnData might already exists.", e);
             }
         }
-
+        if(oldVersion <= 5){
+            database.execSQL("alter table settings add column learningMode INTEGER");
+            database.execSQL("update settings set learningMode='0'");
+        }
+        if (oldVersion <=6){
+            database.execSQL("alter table cards add column imgPath VARCHAR");
+            database.execSQL("update cards set imgPath=''");
+        }
+        if (oldVersion <=7){
+            database.execSQL("alter table cards add column learningDate VARCHAR");
+            database.execSQL("update cards set learningDate='0000-00-00 00:00:00.000000'");
+        }
+        if (oldVersion <= 8){
+            database.execSQL("alter table learning_data add column favourite INTEGER");
+            database.execSQL("update learning_data set favourite='0'");
+        }
 
     }
 
