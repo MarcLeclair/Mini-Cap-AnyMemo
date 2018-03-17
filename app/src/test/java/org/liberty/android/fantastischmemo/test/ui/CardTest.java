475e901c582f0404968260f9f8b0aed0f7baf135
Weieiei <hewei26@yahoo.ca>
2018-03-17 18:46:44 -0400

    #69 change the path from fantastischmemo/test/ui/ to fantastischmemo/ui/

---
+++ app/src/test/java/org/liberty/android/fantastischmemo/test/ui/CardTest.java
@@ -0,0 +1,8 @@
+package org.liberty.android.fantastischmemo.test.ui;
+
+/**
+ * Created by Wei on 3/17/2018.
+ */
+
+public class CardTest {
+}
---
+++ app/src/test/java/org/liberty/android/fantastischmemo/ui/exampleTest.java
@@ -0,0 +1,15 @@
+/**
+ * Created by User on 2018-03-01.
+ */
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+public class exampleTest {
+
+    @Test
+    public void exampleTest(){
+
+        assertTrue(true);
+    }
+}
