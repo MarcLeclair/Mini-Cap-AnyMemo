8b37312861bf58617318a053bafb91f805640a3a
Marc Leclair <marc.leclair0113@gmail.com>
2018-04-15 02:24:09 -0400

    added GameActivity to create a front end connection between initial UI and multiplayer UI

---
+++ app/src/main/java/org/liberty/android/fantastischmemo/ui/MultiPlayerRegistrationFragment.java
@@ -0,0 +1,10 @@
+package org.liberty.android.fantastischmemo.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by User on 2018-04-15.
+ */
+
+public class MultiPlayerRegistrationFragment extends Fragment {
+}
