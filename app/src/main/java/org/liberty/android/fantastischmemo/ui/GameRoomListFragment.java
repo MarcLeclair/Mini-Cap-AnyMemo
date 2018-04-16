62baac2943a37d4a42a0a34939f04ecc67f294e3
Marc Leclair <marc.leclair0113@gmail.com>
2018-04-16 08:09:45 -0400

    #121 created fragment for creating the room

---
+++ app/src/main/java/org/liberty/android/fantastischmemo/ui/GameRoomListFragment.java
@@ -0,0 +1,10 @@
+package org.liberty.android.fantastischmemo.ui;
+
+import org.liberty.android.fantastischmemo.common.BaseFragment;
+
+/**
+ * Created by User on 2018-04-16.
+ */
+
+public class GameRoomListFragment extends BaseFragment {
+}
