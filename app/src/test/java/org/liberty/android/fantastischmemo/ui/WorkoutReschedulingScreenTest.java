a733a3351762a5871b8a8e28245745571714f368
meliduong13 <meliduong13@gmail.com>
2018-04-19 01:37:16 -0400

    #135 Added unit tests including edge cases
    - Added unit test for edge cases unit (Empty dates, input starting with
    many zeros, input that are close to the limit permitted, either above or
    below of 1 unit)
    - Used roboletic to be able to test widget component's input (EditText, TextView, TextInputLayout)
    - Fixed the hard coded id from NotificationService, by using a global
    counter and incrementing every new instance to a different id)

--- app/src/test/java/org/liberty/android/fantastischmemo/ui/WorkoutReschedulingScreenTest.java
+++ app/src/test/java/org/liberty/android/fantastischmemo/ui/WorkoutReschedulingScreenTest.java
@@ -1,5 +1,7 @@
 package org.liberty.android.fantastischmemo.ui;
 
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
 
@@ -7,12 +9,17 @@ import org.junit.runner.RunWith;
 import org.liberty.android.fantastischmemo.BuildConfig;
 import org.liberty.android.fantastischmemo.R;
 import org.liberty.android.fantastischmemo.ui.WorkoutSchedulingScreen;
+import org.liberty.android.fantastischmemo.utils.DateUtil;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,45 +31,162 @@ import static org.junit.Assert.assertTrue;
 
 import org.robolectric.RobolectricGradleTestRunner;
 
+import java.text.ParseException;
+
 @RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21, manifest = "src/main/AndroidManifest.xml",packageName = "org.liberty.android.fantastischmemo")
+@Config(constants = BuildConfig.class, sdk = 21, manifest = "src/main/AndroidManifest.xml", packageName = "org.liberty.android.fantastischmemo")
 public class WorkoutReschedulingScreenTest {
 
     WorkoutSchedulingScreen activity;
     TextInputLayout numDaysInputWrapper;
     TextInputLayout numCardsInputWrapper;
+    TextView startDateMessage;
     String numDaysInput;
     String numCardsInput;
+    int max = 20;
 
     @Before
     public void setUp() {
         activity = Robolectric.buildActivity(WorkoutSchedulingScreen.class)
                 .create()
                 .get();
+        //this is a wrapper on the number of days input
         numDaysInputWrapper = (TextInputLayout) activity.findViewById(R.id
-       .num_days_input_wrapper);
-        numDaysInputWrapper.getEditText().setText("123");
+                .num_days_input_wrapper);
+        //this is a wrapper on the number of cards input
         numCardsInputWrapper = (TextInputLayout) activity.findViewById(R.id
                 .num_cards_input_wrapper);
-        numDaysInput = numDaysInputWrapper.getEditText().getText()
-                .toString();
-       /* secondNumber = (EditText)activity.findViewById(R.id.txtNumber2);
-        addResult = (TextView)activity.findViewById(R.id.txtResult);
-        btnAdd = (Button) activity.findViewById(R.id.btnAdd);*/
+        startDateMessage = (TextView) activity.findViewById(R.id.start_date_message);
     }
 
+    @After
+    public void tearDown(){
+        activity = null;
+        numDaysInputWrapper = null;
+        numCardsInputWrapper = null;
+        startDateMessage = null;
+    }
+
+    @LargeTest
     @Test
-    public void testMainActivityAddition() {
-      /*  //setup
-
-        firstNumber.setText("12.2");
-        secondNumber.setText("13.3");
-
-        //test
-        btnAdd.performClick();
-
-        //verify
-        assertEquals(25.5, Double.parseDouble(addResult.getText().toString()), 0.0);*/
-
+    public void testSetEmptyDate() {
+        startDateMessage.setText("");
+        Assert.assertFalse(activity.validDate(startDateMessage));
     }
+
+    @LargeTest
+    @Test
+    public void testSetMessageDate() {
+        startDateMessage.setText("Must choose a start date");
+        Assert.assertFalse(activity.validDate(startDateMessage));
+    }
+
+    @LargeTest
+    @Test
+    public void testSetCorrectDate() {
+        //the calendar handles the user's date input, therefore the format will always be dd/mm/yyyy
+        startDateMessage.setText("12/04/2070");
+        Assert.assertTrue(activity.validDate(startDateMessage));
+    }
+
+    @LargeTest
+    @Test
+    public void testSetCorrectFarDate() {
+        //the calendar handles the user's date input, therefore the format will always be dd/mm/yyyy
+        startDateMessage.setText("12/04/2000000000");
+        Assert.assertTrue(activity.validDate(startDateMessage));
+    }
+
+    @LargeTest
+    @Test
+    public void testAddNotificationScheduler() {
+        String TAG = getClass().getSimpleName();
+        try {
+            Assert.assertTrue(activity.addNotificationScheduler(DateUtil.getDate(12, 4, 2015), 4, activity));
+        } catch (ParseException e) {
+            Log.e(TAG, "testAddNotificationScheduler: ");
+        }
+    }
+
+    @LargeTest
+    @Test
+    public void testEqualToZeroNumberOfDays() {
+        numDaysInputWrapper.getEditText().setText("0");
+        numCardsInputWrapper.getEditText().setText("0");
+        Assert.assertFalse(activity.validNumCardsOrDays(numCardsInputWrapper, max));
+        Assert.assertFalse(activity.validNumCardsOrDays(numDaysInputWrapper, max));
+    }
+
+    @LargeTest
+    @Test
+    public void testEqualToManyZerosNumberOfDays() {
+        numDaysInputWrapper.getEditText().setText("000000000000000000000000000000000000000");
+        numCardsInputWrapper.getEditText().setText("0000000000000000000000000000000");
+        Assert.assertFalse(activity.validNumCardsOrDays(numCardsInputWrapper, max));
+        Assert.assertFalse(activity.validNumCardsOrDays(numDaysInputWrapper, max));
+    }
+
+    @LargeTest
+    @Test
+    public void testLessThanMaxNumberOfDays() {
+        numDaysInputWrapper.getEditText().setText("19");
+        numCardsInputWrapper.getEditText().setText("19");
+        Assert.assertTrue(activity.validNumCardsOrDays(numCardsInputWrapper, max));
+        Assert.assertTrue(activity.validNumCardsOrDays(numDaysInputWrapper, max));
+    }
+
+    @LargeTest
+    @Test
+    public void testMoreThanMaxNumberOfDays() {
+        numDaysInputWrapper.getEditText().setText("21");
+        numCardsInputWrapper.getEditText().setText("21");
+        Assert.assertFalse(activity.validNumCardsOrDays(numDaysInputWrapper, max));
+        Assert.assertFalse(activity.validNumCardsOrDays(numCardsInputWrapper, max));
+    }
+
+    @LargeTest
+    @Test
+    public void testEmptyNumberOfDays() {
+        numDaysInputWrapper.getEditText().setText("");
+        numCardsInputWrapper.getEditText().setText("");
+        Assert.assertFalse(activity.validNumCardsOrDays(numDaysInputWrapper, max));
+        Assert.assertFalse(activity.validNumCardsOrDays(numCardsInputWrapper, max));
+    }
+
+    @LargeTest
+    @Test
+    public void testEqualToMaxNumberOfDays() {
+        numDaysInputWrapper.getEditText().setText("20");
+        numCardsInputWrapper.getEditText().setText("20");
+        Assert.assertTrue(activity.validNumCardsOrDays(numDaysInputWrapper, max));
+        Assert.assertTrue(activity.validNumCardsOrDays(numCardsInputWrapper, max));
+    }
+
+    @LargeTest
+    @Test
+    public void testStartingWithZerosAndEqualToMaxNumberOfDays() {
+        numDaysInputWrapper.getEditText().setText("00000000000000000020");
+        numCardsInputWrapper.getEditText().setText("000000000000000000000000020");
+        Assert.assertTrue(activity.validNumCardsOrDays(numDaysInputWrapper, max));
+        Assert.assertTrue(activity.validNumCardsOrDays(numCardsInputWrapper, max));
+    }
+
+    @LargeTest
+    @Test
+    public void testStartingWithZerosAndLessToMaxNumberOfDays() {
+        numDaysInputWrapper.getEditText().setText("00000000000000000019");
+        numCardsInputWrapper.getEditText().setText("000000000000000000000000019");
+        Assert.assertTrue(activity.validNumCardsOrDays(numDaysInputWrapper, max));
+        Assert.assertTrue(activity.validNumCardsOrDays(numCardsInputWrapper, max));
+    }
+
+    @LargeTest
+    @Test
+    public void testStartingWithZerosAndMoreThanMaxNumberOfDays() {
+        numDaysInputWrapper.getEditText().setText("00000000000000000000000000000021");
+        numCardsInputWrapper.getEditText().setText("00000000000000000000000000000021");
+        Assert.assertFalse(activity.validNumCardsOrDays(numDaysInputWrapper, max));
+        Assert.assertFalse(activity.validNumCardsOrDays(numCardsInputWrapper, max));
+    }
+
 }
